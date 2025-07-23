08c90cfccf14e9e5a1019fd66e8bb60975ef8349
sadek1999 <sadekali1530@gmail.com>
2025-07-23 22:26:55 +0600

    make component

---
+++ src/Components/shear/nav/Navbar.tsx
@@ -0,0 +1,7 @@
+
+
+export const Navbar = () => {
+  return (
+    <div> This is Navbar .............</div>
+  )
+}
---
+++ src/Components/shear/nav/Navlinks.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const navLinks = () => {
+  return (
+    <div>navLinks</div>
+  )
+}
