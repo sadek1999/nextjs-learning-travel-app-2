33741a625d4c1096f925a956a88b9b00c2776607
sadek1999 <sadekali1530@gmail.com>
2025-08-09 21:45:26 +0600

    set active nav action

--- src/Components/shear/nav/Navbar.tsx
+++ src/Components/shear/nav/Navbar.tsx
@@ -1,13 +1,17 @@
+"use client"
 import Link from "next/link"
 import { NaveLinks } from "./constant"
 import { TbAirBalloon } from "react-icons/tb"
 import { HiBars3BottomRight } from "react-icons/hi2"
-
+import { usePathname } from "next/navigation"
 
 export const Navbar = () => {
+  const pathname = usePathname()
+
   return (
     <nav className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] relative">
       <div className="flex items-center h-full justify-between w-[98%] xl:w-[80%] mx-auto">
+        
         {/* Logo */}
         <div className="flex items-center space-x-2">
           <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center">
@@ -20,25 +24,30 @@ export const Navbar = () => {
 
         {/* Desktop nav */}
         <div className="hidden lg:flex items-center space-x-10">
-          {NaveLinks.map((links) => (
-            <Link href={links.url} key={links.id}>
-              <p
-                className="relative text-white text-base font-medium w-fit block 
-                after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full 
-                after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-right"
-              >
-                {links.label}
-              </p>
-            </Link>
-          ))}
+          {NaveLinks.map((links) => {
+            const isActive = pathname === links.url
+            return (
+              <Link href={links.url} key={links.id}>
+                <p
+                  className={`relative text-base font-medium w-fit block transition-all duration-300 
+                    after:block after:content-[''] after:absolute after:h-[3px] after:w-full 
+                    ${isActive ? "text-yellow-300 after:bg-yellow-300 after:scale-x-100" 
+                               : "text-white after:bg-yellow-300 after:scale-x-0 hover:after:scale-x-100"} 
+                    after:transition-transform after:duration-300 after:origin-right`}
+                >
+                  {links.label}
+                </p>
+              </Link>
+            )
+          })}
         </div>
 
         {/* CTA Button */}
-        <button className=" md:px-2 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg ">
+        <button className="md:px-2 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg">
           Book Now
         </button>
 
-        {/* Burger Menu  */}
+        {/* Burger Menu */}
         <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-white lg:hidden" />
       </div>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
   return (
-   <div>
-<h1>this is root page </h1>
-   </div>
+    <div>
+      <h1>this is root page </h1>
+    </div>
   );
 }
