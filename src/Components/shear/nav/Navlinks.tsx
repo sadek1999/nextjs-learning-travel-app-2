8b1e0fda8e33f4138bba314e82b032aa8544e844
sadek1999 <sadekali1530@gmail.com>
2025-08-09 20:12:17 +0600

    make routes and navbar

--- src/Components/shear/nav/Navlinks.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const navLinks = () => {
-  return (
-    <div>navLinks</div>
-  )
-}
---
+++ src/Components/shear/nav/constant.ts
@@ -0,0 +1,28 @@
+
+export const NaveLinks=[
+    {
+        id:1,
+        url:"/home",
+        label:"Home"
+    },
+    {
+        id:2,
+        url:"/about",
+        label:"About"
+    },
+    {
+        id:3,
+        url:"/contact",
+        label:"Contact"
+    },
+    {
+        id:4,
+        url:"/blogs",
+        label:"Blogs"
+    },
+    {
+        id:5,
+        url:"/destination",
+        label:"Destination"
+    },
+]
